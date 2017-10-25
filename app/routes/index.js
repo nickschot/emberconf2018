c1a7cd4cfc575cd95072e434dd158ce9ca375760
Nick Schot <nickschot@gmail.com>
2017-10-25 21:42:57 +0200

    - Add ember decorators - Add shoebox decorator

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,10 @@
 import Route from '@ember/routing/route';
 import fetch from 'ember-fetch/ajax';
 
+import { shoeboxModel } from 'ember-shoebox-decorator';
+
 export default Route.extend({
+  @shoeboxModel
   model() {
     return fetch('https://api.github.com/users/nickschot')
       .then(function(response) {
