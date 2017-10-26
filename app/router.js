7e9a100f6e85683fddf2d641690294181e6262f9
Nick Schot <nickschot@gmail.com>
2017-10-26 15:49:17 +0200

    Migrate to new route structure

--- app/router.js
+++ app/router.js
@@ -7,6 +7,11 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('posts', function() {
+    this.route('post', {
+      path: ':post_id'
+    });
+  });
 });
 
 export default Router;
