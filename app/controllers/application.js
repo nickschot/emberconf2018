ad8d6379baa2308f911c60775650975ab297f2e9
Nick Schot <nickschot@gmail.com>
2017-10-30 16:57:45 +0100

    Fix responsive rendering when javascript is disabled

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,11 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  fastboot: service(),
+  isFastBoot: computed.reads('fastboot.isFastBoot'),
+
   sideMenuOpen: false,
 
   actions: {
