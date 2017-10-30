7891b3c9d21f35328b5b6345df794f96002f6164
Nick Schot <nickschot@gmail.com>
2017-10-30 21:32:18 +0100

    Update browser targets

--- config/targets.js
+++ config/targets.js
@@ -1,9 +1,9 @@
 /* eslint-env node */
 module.exports = {
   browsers: [
-    'last 1 Chrome versions',
-    'last 1 Firefox versions',
-    'last 1 Safari versions',
-    'last 1 Edge versions'
+    'last 2 Chrome versions',
+    'last 2 Firefox versions',
+    'last 2 Safari versions',
+    'last 2 Edge versions'
   ]
 };
