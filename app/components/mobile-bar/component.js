fa1bd5ccba799be0d28be9e990e54ddf726003f5
Nick Schot <nickschot@gmail.com>
2018-01-10 15:41:21 +0100

    prevent erronous renders by adding standby state

--- app/components/mobile-bar/component.js
+++ app/components/mobile-bar/component.js
@@ -2,11 +2,12 @@ import Component from '@ember/component';
 import { computed, get, set } from '@ember/object';
 import RespondsToScroll from 'ember-responds-to/mixins/responds-to-scroll';
 
-const STATE_CLOSED        = 1;
-const STATE_MOVING_CLOSED = 2;
-const STATE_MOVING        = 3;
-const STATE_MOVING_OPEN   = 4;
-const STATE_OPEN          = 5;
+const STATE_CLOSED         = 1;
+const STATE_MOVING_STANDBY = 2;
+const STATE_MOVING_CLOSED  = 3;
+const STATE_MOVING         = 4;
+const STATE_MOVING_OPEN    = 5;
+const STATE_OPEN           = 6;
 
 export default Component.extend(RespondsToScroll, {
   classNames: ['mobile-bar'],
@@ -29,7 +30,7 @@ export default Component.extend(RespondsToScroll, {
     this._super(...arguments);
 
     if(!get(this, 'isLocked')){
-      get(this, 'wrapperElement').addEventListener('touchstart', () => this.toState(STATE_MOVING), { passive: true });
+      get(this, 'wrapperElement').addEventListener('touchstart', () => this.toState(STATE_MOVING_STANDBY), { passive: true });
       get(this, 'wrapperElement').addEventListener('touchend', () => this.transitionToFinalState(), { passive: true });
     }
 
@@ -46,6 +47,8 @@ export default Component.extend(RespondsToScroll, {
         position: '',
         top: get(this, 'currentPosition')
       });
+    } else if(state === STATE_MOVING_STANDBY){
+      set(this, 'lastScrollTop', this.getScrollTop());
     } else if(state === STATE_MOVING) {
       const options = {
         position: 'absolute'
@@ -79,27 +82,33 @@ export default Component.extend(RespondsToScroll, {
 
   scroll(){
     const currentState = get(this, 'currentState');
-    if(!get(this, 'isLocked')
-      && ( currentState === STATE_MOVING
-        || currentState === STATE_MOVING_CLOSED
-        || currentState === STATE_MOVING_OPEN)
-    ){
+    if(!get(this, 'isLocked')){
       const scrollTop = this.getScrollTop();
-      const dy = scrollTop - get(this, 'lastScrollTop');
 
+      // MOVING_STANDBY prevents jumping around on small movements, acts as a deadzone
+      if(currentState === STATE_MOVING_STANDBY){
+        const dy = scrollTop - get(this, 'lastScrollTop');
 
-      if(dy === 0) {
-        return;
+        set(this, 'currentPosition', get(this, 'currentPosition') - dy);
+        this.toState(STATE_MOVING);
+      } else if(currentState === STATE_MOVING
+             || currentState === STATE_MOVING_CLOSED
+             || currentState === STATE_MOVING_OPEN
+      ){
+        const dy = scrollTop - get(this, 'lastScrollTop');
+
+        if(dy === 0) {
+          return;
+        }
+
+        set(this, 'lastScrollTop', scrollTop);
+        this.setPosition(dy, currentState);
       }
-
-      set(this, 'lastScrollTop', scrollTop);
-      this.setPosition(dy, currentState);
     }
   },
 
-  setPosition(dy, currentState, setAsNewPosition = false){
-    let newPosition = setAsNewPosition ? dy : get(this, 'currentPosition') + dy;
-
+  setPosition(dy, currentState){
+    let newPosition = get(this, 'currentPosition') + dy;
 
     if(newPosition <= 0){
       if(currentState !== STATE_MOVING_OPEN){
