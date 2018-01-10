0965e43a5fdf209d70658f598ba9227b6207ec29
Nick Schot <nickschot@gmail.com>
2018-01-10 14:45:23 +0100

    better implementation of the mobile bar "state machine"

--- app/components/mobile-bar/component.js
+++ app/components/mobile-bar/component.js
@@ -1,27 +1,25 @@
 import Component from '@ember/component';
 import { computed, get, set } from '@ember/object';
 import RespondsToScroll from 'ember-responds-to/mixins/responds-to-scroll';
-import { task, timeout } from 'ember-concurrency';
 
-const DIRECTION_DOWN = 1;
-const DIRECTION_UP = 2;
+const STATE_CLOSED        = 1;
+const STATE_MOVING_CLOSED = 2;
+const STATE_MOVING        = 3;
+const STATE_MOVING_OPEN   = 4;
+const STATE_OPEN          = 5;
 
 export default Component.extend(RespondsToScroll, {
   classNames: ['mobile-bar'],
   classNameBindings: [
-    'isBottomBar:mobile-bar__bottom:mobile-bar__top',
-    'isTransitioning:mobile-bar__transitioning',
-    //'isMoving:mobile-bar__moving'
+    'isBottomBar:mobile-bar__bottom:mobile-bar__top'
   ],
 
   isLocked: true,
   height: 50,
 
-  lastScrollTop: 0.0,
+  currentState: STATE_OPEN,
   currentPosition: 0,
-  isTransitioning: false,
-  isMoving: false,
-  direction: DIRECTION_DOWN,
+  lastScrollTop: 0,
 
   isBottomBar: computed('type', function(){
     return get(this, 'type') === 'bottom';
@@ -31,118 +29,96 @@ export default Component.extend(RespondsToScroll, {
     this._super(...arguments);
 
     if(!get(this, 'isLocked')){
-      //get(this, 'wrapperElement').addEventListener('touchstart', () => this.startMoving(), { passive: true });
-      get(this, 'wrapperElement').addEventListener('touchend', () => this.stopMoving(), { passive: true });
+      get(this, 'wrapperElement').addEventListener('touchstart', () => this.toState(STATE_MOVING), { passive: true });
+      get(this, 'wrapperElement').addEventListener('touchend', () => this.transitionToFinalState(), { passive: true });
     }
 
     set(this, 'lastScrollTop', this.getScrollTop());
   },
 
-  startMoving(){
-    this.$().css({
-      position: 'absolute',
-      top: this.getScrollTop()
-    });
+  toState(state){
+    const currentState = get(this, 'currentState');
 
-    set(this, 'isMoving', true);
+    if(state === STATE_OPEN || state === STATE_MOVING_OPEN) {
+      set(this, 'currentPosition', 0);
+
+      this.$().css({
+        position: '',
+        top: get(this, 'currentPosition')
+      });
+    } else if(state === STATE_MOVING) {
+      const options = {
+        position: 'absolute'
+      };
+
+      if (currentState !== STATE_MOVING) {
+        options.top = this.getScrollTop() - get(this, 'currentPosition');
+      }
+
+      this.$().css(options);
+    } else if(state === STATE_CLOSED || state === STATE_MOVING_CLOSED){
+      set(this, 'currentPosition', get(this, 'height'));
+
+      this.$().css({
+        position: '',
+        top: -get(this, 'currentPosition')
+      });
+    }
+
+    set(this, 'currentState', state);
   },
-  stopMoving(){
-    this.$().css({
-      position: '',
-      top: ''
-    });
+  transitionToFinalState(){
+    const finalState = get(this, 'currentPosition') > get(this, 'height') / 2
+      ? STATE_CLOSED
+      : STATE_OPEN;
 
-    const newScrollTop = this.getScrollTop() - get(this, 'lastScrollTop');
-    console.log(this.getScrollTop(), get(this, 'lastTop'));
-    set(this, 'lastScrollTop', newScrollTop);
-    set(this, 'isMoving', false);
+    //TODO: transition
+
+    this.toState(finalState);
   },
 
   scroll(){
-    if(!get(this, 'isLocked')){
-      set(this, 'isTransitioning', false);
-
+    const currentState = get(this, 'currentState');
+    if(!get(this, 'isLocked')
+      && ( currentState === STATE_MOVING
+        || currentState === STATE_MOVING_CLOSED
+        || currentState === STATE_MOVING_OPEN)
+    ){
       const scrollTop = this.getScrollTop();
-      let dy = scrollTop - get(this, 'lastScrollTop');
+      const dy = scrollTop - get(this, 'lastScrollTop');
+
 
       if(dy === 0) {
         return;
       }
 
       set(this, 'lastScrollTop', scrollTop);
-
-      this.setPosition(dy);
-      //get(this, 'setOpenOrClose').perform();
+      this.setPosition(dy, currentState);
     }
   },
 
-  setPosition(dy, setAsNewPosition = false){
+  setPosition(dy, currentState, setAsNewPosition = false){
     let newPosition = setAsNewPosition ? dy : get(this, 'currentPosition') + dy;
 
+
     if(newPosition <= 0){
-      newPosition = 0;
-      this.stopMoving();
+      if(currentState !== STATE_MOVING_OPEN){
+        this.toState(STATE_MOVING_OPEN);
+      }
+
       return;
     } else if(newPosition >= get(this, 'height')){
-      newPosition = get(this, 'height');
+      if(currentState !== STATE_MOVING_CLOSED){
+        this.toState(STATE_MOVING_CLOSED);
+      }
 
-      this.$().css({
-        position: '',
-        top: `${-get(this, 'height')}px`
-      });
-
-      set(this, 'isHidden', true);
       return;
-    } else {
-      // start moving if we haven't started yet
-      if(!get(this, 'isMoving')){
-        this.startMoving();
-      }
-
-      if(get(this, 'isHidden')){
-        const newTop = get(this, 'lastScrollTop') - get(this, 'height');
-        console.log(newTop);
-
-        this.$().css({
-          position: 'absolute',
-          top: `${newTop}px`
-        });
-
-        set(this, 'isHidden', false);
-        set(this, 'lastTop', newTop);
-      }
+    } else if(get(this, 'currentState') !== STATE_MOVING) {
+      this.toState(STATE_MOVING);
     }
 
     set(this, 'currentPosition', newPosition);
-
-    /*if(!get(this, 'isBottomBar')){
-      newPosition = -newPosition;
-    }*/
-
-    //this.element.style.transform = `translateY(${newPosition}px)`;
-    //this.$().css('transform', `translateY(${newPosition}px)`);
   },
-
-  /**
-   * Decide whether the bar should be fully visible or completely hidden
-   * @param position
-   */
-  setOpenOrClose: task(function * () {
-    yield timeout(450);
-
-    set(this, 'isTransitioning', true);
-
-    const newPosition = get(this, 'currentPosition') >= get(this, 'height')/2 ? get(this, 'height') : 0;
-    this.setPosition(newPosition, true);
-
-    get(this, 'disableTransition').perform();
-  }).restartable(),
-
-  disableTransition: task(function * () {
-    yield timeout(150);
-
-    set(this, 'isTransitioning', false);
-  }),
 
   getScrollTop(){
     return document.scrollingElement.scrollTop || document.documentElement.scrollTop;
