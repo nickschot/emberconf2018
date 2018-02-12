df4c93f09f91f6b9c51aa1ed174fcb2a8d5497c3
Nick Schot <nickschot@gmail.com>
2018-02-12 22:48:37 +0100

    Detect transition and immediately stop rendering the elsewhere

--- app/routes/posts/index.js
+++ app/routes/posts/index.js
@@ -3,5 +3,17 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     return this.modelFor('posts');
+  },
+
+  setupController(controller, model) {
+    this._super(controller, model);
+
+    controller.set('elseWhereIsActive', true);
+  },
+
+  actions: {
+    willTransition(){
+      this.controller.set('elseWhereIsActive', false);
+    }
   }
 });
