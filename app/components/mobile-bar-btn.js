b3ce1f1b5013c95accb8ce8035c7c21ff4ba44ae
Nick Schot <nickschot@gmail.com>
2018-02-27 21:41:02 +0100

    make mobile-bar-btn component more generic fix transitions fix stuttering animations on android

--- app/components/mobile-bar-btn.js
+++ app/components/mobile-bar-btn.js
@@ -1,10 +1,12 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  classNames: ['mobile-menu__toggle'],
+  classNames: ['mobile-bar__btn'],
+  classNameBindings: ['scaleIcon:mobile-bar__icon-btn--scale-icon'],
 
   targetRoute: '',
   action: null,
   icon: '',
+  scaleIcon: false,
   text: ''
 });
