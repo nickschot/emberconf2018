bf9ee4ea7a5b94c3c507cbddb24d3e366d53ee07
Nick Schot <nickschot@gmail.com>
2018-02-28 23:23:44 +0100

    Elsewhere more titles Generate dummy new page with transition Clean up application controller Add motion service and set-body-class to application controller Initial post transition work

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,18 +3,7 @@ import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
+  motion: service('-ea-motion'),
   fastboot: service(),
   isFastBoot: computed.reads('fastboot.isFastBoot'),
-
-  //TODO: URL change during close makes Android hang for a bit, do this after animation finished
-  //queryParams: ['sideMenuOpen'],
-
-  sideMenuOpen: false, // TODO: possible fix for the above is to make this a get/set
-  sideMenuNewState: computed.not('sideMenuOpen'),
-
-  actions: {
-    toggleSideMenu() {
-      this.set('sideMenuOpen', !this.get('sideMenuOpen'));
-    }
-  }
 });
