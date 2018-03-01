6e8fde5dd99265ec431d72c1b35431694aa3ef9e
Nick Schot <nickschot@gmail.com>
2018-03-01 15:18:50 +0100

    Use new infinite-scroller implementation

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -1,9 +1,24 @@
 import Controller from '@ember/controller';
 
 import move from 'ember-animated/motions/move';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
-  transition
+  transition,
+
+  currentModelIndex: computed('modelCollection.[]', 'model', function(){
+    return this.get('modelCollection').indexOf(this.get('model'));
+  }),
+  previousModel: computed('modelCollection.[]', 'currentModelIndex', function(){
+    return this.get('currentModelIndex') > 0
+      ? this.get('modelCollection').objectAt(this.get('currentModelIndex') - 1)
+      : null;
+  }),
+  nextModel: computed('modelCollection.[]', 'currentModelIndex', function(){
+    return this.get('currentModelIndex') + 1 < this.get('modelCollection.length')
+      ? this.get('modelCollection').objectAt(this.get('currentModelIndex') + 1)
+      : null;
+  }),
 });
 
 function transition(){
