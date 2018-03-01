774cbef8368009f991ce3a06cadb4f3fe726ae49
Nick Schot <nickschot@gmail.com>
2018-03-01 10:53:30 +0100

    Update breakpoints to match with bs4 defaults

--- app/breakpoints.js
+++ app/breakpoints.js
@@ -1,5 +1,8 @@
+// Bootstrap 4.0.0 default breakpoints
 export default {
-  mobile:  '(max-width: 767px)',
-  tablet:  '(min-width: 768px) and (max-width: 991px)',
-  desktop: '(min-width: 992px) and (max-width: 1200px)'
+  xs: '(max-width: 575px)',
+  sm: '(min-width: 576px) and (max-width: 767px)',
+  md: '(min-width: 768px) and (max-width: 991px)',
+  lg: '(min-width: 992px) and (max-width: 1199px)',
+  xl: '(min-width: 1200px)'
 };
