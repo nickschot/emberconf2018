4805b9acf5f079b8ab20e4a5d3a3bb990e6c38a0
Nick Schot <nickschot@gmail.com>
2018-03-03 16:30:45 +0100

    add initial impl. of a parallax element remove fastboot

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,9 +1,8 @@
 import DS from 'ember-data';
 import config from 'ember-fastboot-blog/config/environment';
 import AdapterFetch from 'ember-fetch/mixins/adapter-fetch';
-import CachedShoe from 'ember-cached-shoe';
 
-export default DS.JSONAPIAdapter.extend(AdapterFetch, CachedShoe, {
+export default DS.JSONAPIAdapter.extend(AdapterFetch, {
   host: config.host,
   coalesceFindRequests: true,
 });
--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,7 +3,5 @@ import { computed } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default Controller.extend({
-  motion: service('-ea-motion'),
-  fastboot: service(),
-  isFastBoot: computed.reads('fastboot.isFastBoot'),
+  motion: service('-ea-motion')
 });
