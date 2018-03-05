ae729758663809ca987f3ba642d523675d471a34
Nick Schot <nickschot@gmail.com>
2018-03-05 16:23:35 +0100

    load dummy data into store in application route peek from stores in other routes

--- app/routes/home/posts.js
+++ app/routes/home/posts.js
@@ -2,8 +2,6 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return this.store.query('post', {
-      include: 'author'
-    });
+    return this.store.peekAll('post');
   }
 });
