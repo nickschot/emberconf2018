bcb4aa4be3a35ae1ec960e0d741ea0278a7a0c9c
Nick Schot <nickschot@gmail.com>
2019-10-11 23:33:16 +0200

    run angle bracket codemod, workaround mobile-pane issue

--- app/controllers/home/media.js
+++ app/controllers/home/media.js
@@ -1,8 +1,14 @@
-import classic from 'ember-classic-decorator';
 import Controller from '@ember/controller';
+import { action } from '@ember/object';
 
-@classic
 export default class MediaController extends Controller {
   queryParams = ['pane'];
   pane = 0;
+
+  @action
+  updatePane(index) {
+    // TODO: this is a workaround for issue on mobile-pane master
+    this.set('pane', typeof index === 'object' ? index.index : index);
+  }
+
 }
